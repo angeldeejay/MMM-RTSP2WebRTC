5d2b91e8311c7a2c0f30ccbd372259b3a4cdb6a4
Andrés Vanegas <ukab72106@gmail.com>
2026-05-11 19:48:35 -0500

    feat: update VideoRTC and VideoStream classes for improved functionality and Safari compatibility

--- js/video-stream.js
+++ js/video-stream.js
@@ -1,93 +1,81 @@
 class VideoStream extends VideoRTC {
-  constructor() {
-    super();
-
-    this.DISCONNECT_TIMEOUT = 5000;
-    this.RECONNECT_TIMEOUT = 10000;
-
-    this.background = true;
-    this.visibilityCheck = false;
-  }
-
-  set overlay(value) {
-    let overlay = this.querySelector(".overlay");
-    if (!overlay) overlay = this.createOverlay();
-    if (value === true) {
-      overlay.style.opacity = 1;
-    } else {
-      overlay.style.opacity = 0;
+    constructor() {
+        super();
+        this.DISCONNECT_TIMEOUT = 5000;
+        this.RECONNECT_TIMEOUT = 10000;
+        this.background = true;
+        this.visibilityCheck = false;
     }
-  }
 
-  createOverlay() {
-    if (!this.querySelector(".overlay"))
-      this.innerHTML = '<div class="overlay"></div>';
-    return this.querySelector(".overlay");
-  }
-
-  /**
-   * Custom GUI
-   */
-  oninit() {
-    Log.debug(this.constructor.name, "stream.oninit", this);
-    this.overlay = true;
-    super.oninit();
-    this.video.controls = false;
-  }
-
-  onconnect() {
-    Log.debug(this.constructor.name, "stream.onconnect", this);
-    const result = super.onconnect();
-    if (result) {
-      Log.debug(this.constructor.name, "loading", this);
-      this.overlay = true;
+    _showOverlay() {
+        if (this._overlay) this._overlay.style.opacity = '1';
     }
-    return result;
-  }
 
-  ondisconnect() {
-    Log.debug(this.constructor.name, "stream.ondisconnect", this);
-    this.overlay = true;
-    super.ondisconnect();
-  }
-
-  onopen() {
-    Log.debug(this.constructor.name, "stream.onopen", this);
-    this.overlay = true;
-    const result = super.onopen();
-
-    this.onmessage["stream"] = (msg) => {
-      Log.debug(this.constructor.name, "stream.onmessge", msg, this);
-      switch (msg.type) {
-        case "error":
-          Log.warn(msg.value, this);
-          break;
-        case "mse":
-        case "hls":
-        case "mp4":
-        case "mjpeg":
-          Log.debug(this.constructor.name, msg.type.toUpperCase(), this);
-          break;
-      }
-    };
-
-    return result;
-  }
-
-  onclose() {
-    Log.debug(this.constructor.name, "stream.onclose", this);
-    return super.onclose();
-  }
-
-  onpcvideo(ev) {
-    Log.debug(this.constructor.name, "stream.onpcvideo", this);
-    super.onpcvideo(ev);
-
-    if (this.pcState !== WebSocket.CLOSED) {
-      Log.debug(this.constructor.name, "RTC", this);
-      this.overlay = false;
+    _hideOverlay() {
+        if (this._overlay) this._overlay.style.opacity = '0';
     }
-  }
+
+    oninit() {
+        Log.debug(this.constructor.name, 'stream.oninit', this);
+        super.oninit();
+        this.video.controls = false;
+
+        this._overlay = document.createElement('div');
+        this._overlay.classList.add('video-stream-overlay');
+        this._overlay.style.opacity = '1';
+        this.appendChild(this._overlay);
+
+        // Hide overlay when MSE/HLS stream has data
+        this.video.addEventListener('loadeddata', () => this._hideOverlay());
+    }
+
+    onconnect() {
+        Log.debug(this.constructor.name, 'stream.onconnect', this);
+        const result = super.onconnect();
+        if (result) this._showOverlay();
+        return result;
+    }
+
+    ondisconnect() {
+        Log.debug(this.constructor.name, 'stream.ondisconnect', this);
+        this._showOverlay();
+        super.ondisconnect();
+    }
+
+    onopen() {
+        Log.debug(this.constructor.name, 'stream.onopen', this);
+        const result = super.onopen();
+
+        this.onmessage['stream'] = msg => {
+            Log.debug(this.constructor.name, 'stream.onmessage', msg, this);
+            switch (msg.type) {
+                case 'error':
+                    Log.warn(msg.value, this);
+                    break;
+                case 'mse':
+                case 'hls':
+                case 'mp4':
+                case 'mjpeg':
+                    Log.debug(this.constructor.name, msg.type.toUpperCase(), this);
+                    break;
+            }
+        };
+
+        return result;
+    }
+
+    onclose() {
+        Log.debug(this.constructor.name, 'stream.onclose', this);
+        return super.onclose();
+    }
+
+    onpcvideo(video2) {
+        Log.debug(this.constructor.name, 'stream.onpcvideo', this);
+        super.onpcvideo(video2);
+        if (this.pcState !== WebSocket.CLOSED) {
+            this._hideOverlay();
+        }
+    }
 }
 
-customElements.define("video-stream", VideoStream);
+customElements.define('video-stream', VideoStream);
